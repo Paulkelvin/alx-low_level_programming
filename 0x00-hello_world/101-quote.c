23af939d0a4f88e6565e1d60621ebcca5eebdf58
Paulkelvin <ibukunadesanya0@gmail.com>
2023-02-16 07:52:33 -0800

    Task8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/*
+ * main - prints an error message to stderr
+ * Return: Returns 1 to indicate an error occurred.
+ */
+int main(void)
+{
+char str[] = "and that piece of art is useful\" - DoraKorpar, 2015-10-19\n";
+fwrite(str, sizeof(str) - 1, 1, stderr);
+return (1);
+}
+
