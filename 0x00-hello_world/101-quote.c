9fb589305c4e5dd5deca8da746c9e9cd15d431ad
Paulkelvin <ibukunadesanya0@gmail.com>
2023-02-16 08:09:18 -0800

    Task8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,8 +5,7 @@
  */
 int main(void)
 {
-char str[] = "and that piece of art is useful\" - DoraKorpar, 2015-10-19\n";
-fwrite(str, sizeof(str) - 1, 1, stderr);
+fput("and that piece of art is useful\" - DoraKorpar, 2015-10-19\n", stdout);
 return (1);
 }
 
