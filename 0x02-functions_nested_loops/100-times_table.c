5b6f23c9176c30f3eee37347b2f078d0138ca1a5
Paulkelvin <ibukunadesanya0@gmail.com>
2023-02-21 07:03:09 -0800

    Task12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,47 +1,36 @@
-#include "main.h"
-
+#include <stdio.h>
 /**
- * print_times_table - prints the n times table, starting with 0
- * @n: number of the times table
+ * main - prints out all the numbers between 00 and 99
+ * with no two digits being the same
+ * Return: 0
  */
-void print_times_table(int n)
+int main(void)
 {
-	int i, j, k;
+	int i, j;
 
-	if (n >= 0 && n <= 15)
+	for (i = 48; i < 58; i++)
 	{
-		for (i = 0; i <= n; i++)
+		for (j = i; j < 58; j++)
 		{
-			for (j = 0; j <= n; j++)
+			if (i == j)
 			{
-				k = j * i;
-				if (j == 0)
-				{
-					_putchar(k + '0');
-				} else if (k < 10 && j != 0)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(k + '0');
-				} else if (k >= 10 && k < 100)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar((k / 10) + '0');
-					_putchar((k % 10) + '0');
-				} else if (k >= 100)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar((k / 100) + '0');
-					_putchar(((k / 10) % 10) + '0');
-					_putchar((k % 10) + '0');
-				}
+				continue;
 			}
-			_putchar('\n');
+
+			putchar(i);
+			putchar(j);
+
+			if (i == 56 && j == 57)
+			{
+				break;
+			}
+			else
+			{
+				putchar(',');
+				putchar(' ');
+			}
 		}
 	}
+	putchar('\n');
+	return (0);
 }
